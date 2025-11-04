1a389932c5d0cab3931a1ef8c32c5ff8954f3476
snx2 <lunguj665@gmail.com>
2025-11-04 20:11:19 +0200

    added colors to the version

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #the entry files
 import typer
+from rich import print
 
 app = typer.Typer()
+
 @app.command()
 def version(version: str):
-    print(f"xyra calculator version pre:0.1")
+    print(f"xyrculator version [bold red]pre:[green]0.1")
 
 
 if __name__ == "__main__":
