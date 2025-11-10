1593308d822a96d2418251a32729de5ae77ff21a
snx2 <lunguj665@gmail.com>
2025-11-10 06:50:29 +0200

    fixed file structure with cide

--- utils/memory.py
+++ utils/memory.py
@@ -1,1 +1,18 @@
-# this is where th memory management code shoukd be 
+# this is where th memory management code shoukd be
+history = []
+
+# function to add entry to history
+
+
+def add_to_history(calculation):
+    history.append(calculation)
+# function to display all history
+
+
+def show_history():
+    if not history:
+        print("No history yet.")
+    else:
+        print("Calculation History:")
+        for item in history:
+            print(item)
